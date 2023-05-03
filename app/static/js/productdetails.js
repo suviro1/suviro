a82902e6a04d281fc1c59ae86a71f6627061f1ee
suviro <sales@suviro.in>
2023-05-03 13:32:37 +0000

    hosted updates

--- app/static/js/productdetails.js
+++ app/static/js/productdetails.js
@@ -11,6 +11,7 @@ imgBtns.forEach((imgItem) => {
 });
 
 function slideImage(){
+    console.log('hello')
     const displayWidth = document.querySelector('.img-showcase img:first-child').clientWidth;
 
     document.querySelector('.img-showcase').style.transform = `translateX(${- (imgId - 1) * displayWidth}px)`;
